d5fd16bb3ac0bc5c22ebfa52b66d85aa25655550
Barbarosa10 <duciucdanut10@gmail.com>
2023-03-17 20:42:37 +0200

    addes media for small screens and added js for menu list

---
+++ js/script.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    const toggleButton = document.getElementById('toggle-button')
+    const naviList = document.getElementById('navi-list')
+
+    toggleButton.addEventListener('click', () =>{
+        naviList.classList.toggle('active');
+    })
+}
+
+
+
