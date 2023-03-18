4e387542ef3477a78dc964a48d7e384df1d9a2e7
Barbarosa10 <duciucdanut10@gmail.com>
2023-03-18 12:47:46 +0200

    changeImg function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
-function menuClick(){
-    // let toggleButton = document.getElementById('toggle-button')
-    let naviList = document.getElementById('navi-list')
-    naviList.classList.toggle('active');
+function menuClick() {
+  // let toggleButton = document.getElementById('toggle-button')
+  let naviList = document.getElementById('navi-list')
+  naviList.classList.toggle('active');
 }
 
 let slideIndex = 1;
@@ -19,19 +19,32 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  //   let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-  slides[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " active";
+  //   for (i = 0; i < dots.length; i++) {
+  //     dots[i].className = dots[i].className.replace(" active", "");
+  //   }
+  slides[slideIndex - 1].style.display = "block";
+  //   dots[slideIndex-1].className += " active";
 }
+let slide = 1;
+function changeImg() {
+  let slides = document.getElementsByClassName("mySlides");
+  let i;
+  if (slideIndex > slides.length) { slideIndex = 1 }
 
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+  slideIndex += 1;
+  setTimeout("changeImg()", 4000)
+}
+window.onload = changeImg;
 
 
 
