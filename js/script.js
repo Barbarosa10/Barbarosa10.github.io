03af89b811f21adb42fc71b1d8f3fd18489ff36b
Barbarosa10 <duciucdanut10@gmail.com>
2023-03-19 15:23:37 +0200

    added phone/whatsapp icon animation

--- js/script.js
+++ js/script.js
@@ -48,4 +48,43 @@ function changeImg() {
 window.onload = changeImg;
 
 
-
+function phone(){
+  let phone = document.getElementById("phone");
+  setTimeout(function () {
+    phone.style = "font-size: 49px;";
+  }, 500);
+setTimeout(function () {
+    phone.style = "font-size: 50px;";
+  }, 550);
+setTimeout(function () {
+    phone.style = "font-size: 51px;";
+  }, 600);
+  setTimeout(function () {
+    phone.style = "font-size: 52px;";
+  }, 650);
+  setTimeout(function () {
+    phone.style = "font-size: 53px;";
+  }, 700);
+}
+function whatsapp(){
+  let phone = document.getElementById("whatsapp");
+  setTimeout(function () {
+    phone.style = "font-size: 49px;";
+  }, 500);
+setTimeout(function () {
+    phone.style = "font-size: 50px;";
+  }, 550);
+setTimeout(function () {
+    phone.style = "font-size: 51px;";
+  }, 600);
+  setTimeout(function () {
+    phone.style = "font-size: 52px;";
+  }, 650);
+  setTimeout(function () {
+    phone.style = "font-size: 53px;";
+  }, 700);
+}
+phone();
+setInterval(phone, 700);
+whatsapp();
+setInterval(whatsapp, 700);
